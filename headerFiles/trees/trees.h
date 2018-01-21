60c46800f4b6a64c3e47605312cd2f05543ea81a
Raman Tehlan <ramantehlan@gmail.com>
2018-01-21 09:02:55 +0530

    Trees setup

--- headerFiles/trees/trees.h
+++ headerFiles/trees/trees.h
@@ -8,8 +8,127 @@
 #ifndef TREES_H
 #define TREES_H
 
+#include <stdio.h>
+#include <stdlib.h>
 
+class binarySearchTree{
+    //Structure of a single node
+    class node{
+    public:
+        int data;
+        node* left;
+        node* right;
+    }; 
 
+public:
+    node* root = NULL;
+    
+    binarySearchTree(){
+        
+    }
+    
+    //To insert a node into the BST
+    void insert(int value){
+        node* ele = NULL;
+        node* currentNode = root;
+        node* parentNode = NULL;
+        
+        ele = (node*)malloc(sizeof(node));
+        ele->data = value;
+        ele->left = NULL;
+        ele->right = NULL;
+        
+        if(root == NULL){
+            root = ele;
+            root->left = root->right = NULL;
+        }else{
+            while(currentNode != NULL){
+                parentNode = currentNode;
+                if(currentNode->data > value)
+                    currentNode = currentNode->left;
+                else
+                    currentNode = currentNode->right;
+            }
+            if(parentNode->data > value)
+                parentNode->left = ele;
+            else 
+                parentNode->right = ele;
+        } 
+    }
+    
+    //Pre-order traversal 
+    void preOrderAccess(node* n){
+        if(n != NULL){
+            printf("%d ", n->data);
+            preOrderAccess(n->left);
+            preOrderAccess(n->right);
+        }
+    }
+    
+    //In-order traversal
+    void inOrderAccess(node* n){
+        if(n != NULL){
+            inOrderAccess(n->left);
+            printf("%d ", n->data);
+            inOrderAccess(n->right);
+        }
+    }
+    
+    //Post-order traversal
+    void postOrderAccess(node* n){
+        if(n != NULL){
+            postOrderAccess(n->left);
+            postOrderAccess(n->right);
+            printf("%d ", n->data);
+        }
+    }
+    
+    //smallest node Access
+    void smallestData(){
+        if(root != NULL){
+            node* leftNode = smallestNode(root);
+            printf("%d ", leftNode->data);
+        }
+    }
+    
+    //Largest node access
+    void largestData(){
+        if(root != NULL){
+            node* rightNode = largestNode(root);
+            printf("%d ", rightNode->data);
+        }
+    }
+    
+    //Most left node
+    node* smallestNode(node* n){
+        if(n == NULL || n->left == NULL){
+            return n;
+        }else{
+            return smallestNode(n->left);
+        }
+    }
+    
+    //Most right node
+    node* largestNode(node* n){
+        if(n == NULL || n->right == NULL){
+            return n;
+        }else{
+            return largestNode(n->right);
+        }
+    }
+    
+    //To get the root of BST
+    node* getRoot(){
+        return root;
+    }
+    
+    //To display the BST
+    void print(){
+        
+
+    }
+    
+};
 
 #endif /* TREES_H */
 
--- main.cpp
+++ main.cpp
@@ -9,27 +9,59 @@
 #include <iostream>
 #include <stdlib.h>
 
+#include "headerFiles/trees/trees.h"
+
 using namespace std;
 
 // DEFAULT GLOBAL VARIABLES
+// _Warning: Do not change the position of any option_
 string exitOption = "Exit (X)";
 string backOption = "Back (<-)";
 string mainOption[] = {
-    "Linked Lists", "queues", "stacks", "trees", "graphs", "heaps"
+    "Linked Lists", "queues", "stacks", 
+    "trees", "graphs", "heaps"
 };
-string linkedListOption[] = {"singly LL", "circular LL", 
-                             "doubly LL", "circular LL", "header LL"};
-string treeOption[] = {"Binary Tree" , "Binary Search Tree"};
+string linkedListOption[] = {
+    "singly LL", "circular LL", "doubly LL", 
+    "circular LL", "header LL"
+};
+string treeOption[] = {
+    "Binary Tree" , "Binary Search Tree"
+};
+string binarySearchTreeOption[] = {
+    "Insert a new node", "Pre-order traversal", "In-order traversal", 
+    "Post-order traversal", "Find smallest node", "Find Largest node", 
+    "Delete a node", "Count number of nodes", "Count non-leaf/internal nodes", 
+    "Count leaf/external nodes",  "Calculate hight", "Find the mirror Image",
+    "Find path to a node", "Delete the tree"
+};
 
 // DECLARING FUNCTIONS
 void menuDriven(string[] , int);
 void doAction(string);
+
+// IMPORTANT DATA STRUCTURES
+binarySearchTree BST = binarySearchTree();
+
+
+
+
 
 /**
  * MAIN PROGRAM START HERE
  * @return 0
  */
 int main() {    
+// To fill dummy data in BST
+BST.insert(54);
+BST.insert(50);
+BST.insert(32);
+BST.insert(64);
+BST.insert(24);
+BST.insert(43);
+BST.insert(15);
+BST.insert(12);
+
     menuDriven(mainOption , sizeof(mainOption));       
     return 0;
 }
@@ -39,21 +71,22 @@ void menuDriven(string menu[] , int size){
     // adding 2 since exit and back options are added by default
     size = size/sizeof(menu[0]);
     // Run till we reach end of array
-    printf("----------------------------\n");
-    cout << "[" << 0 << "] " << exitOption << endl;
-    cout << "[" << 1 << "] " << backOption << endl;
-    printf("----------------------------\n");
+    printf("\t----------------------------\n");
+    cout << "\t[" << 0 << "] " << exitOption << endl;
+    cout << "\t[" << 1 << "] " << backOption << endl;
+    printf("\t----------------------------\n");
     for(int l = 0; l < size; l++){
-        cout << "[" << l + 2 << "] " << menu[l] << endl;
+        cout << "\t[" << l + 2 << "] " << menu[l] << endl;
     }
-    printf("----------------------------\n");
+    printf("\t----------------------------\n");
     
     // Let user choose from the menu
     int option;
     do{
         // Getting user answer
-        printf("Enter Your Option:  ");
+        printf("\tEnter Your Option:  ");
         scanf("%d",&option);
+        cout << endl;
         
         // Check if option selected is not out of range
         if(option > (size+1) || option < 0){
@@ -71,39 +104,103 @@ void menuDriven(string menu[] , int size){
 }
 
 void doAction(string actionName){
+        //Exit the program
         if(actionName == exitOption){
             printf("\n> Thank You for stopping by. \n");
             printf("> Creator : ramantehlan@github \n");
             exit(0);
-        }else if(actionName == backOption){
+        }
+        //Going back to main menu
+        else if(actionName == backOption){
             printf("> Going back to main menu. \n");
             menuDriven(mainOption , sizeof(mainOption));
         }
          
+        //Linked list
         else if(actionName == mainOption[0]){
              printf("> Linked List selected \n");
              menuDriven(linkedListOption , sizeof(linkedListOption));
-        }else if(actionName == mainOption[1]){
+        }
+        //Queues
+        else if(actionName == mainOption[1]){
              printf("> Queues selected \n");
-        }else if(actionName == mainOption[2]){
+        }
+        //Stack
+        else if(actionName == mainOption[2]){
              printf("> Stacks selected \n");
-        }else if(actionName == mainOption[3]){
+        }
+        //Trees
+        else if(actionName == mainOption[3]){
              printf("> Trees selected \n");
              menuDriven(treeOption , sizeof(treeOption));
-        }else if(actionName == mainOption[4]){
+        }
+        //Graphs
+        else if(actionName == mainOption[4]){
              printf("> Graphs selected \n");
-        }else if(actionName == mainOption[5]){
+        }
+        //Heaps
+        else if(actionName == mainOption[5]){
              printf("> Heaps selected \n");
         }
         
+        //Binary tree
         else if(actionName == treeOption[0]){
-            #include "headerFiles/trees/trees.h"
             printf("> Binary tree \n");
-        }else if(actionName == treeOption[1]){
-            #include "headerFiles/trees/trees.h"
-            printf("> Binary search tree \n");
+        }
+        //Binary search tree
+        else if(actionName == treeOption[1]){
+            printf("> Created binary search tree (BST)\n");
+            //Show all the possible actions on binary search tree
+            menuDriven(binarySearchTreeOption, sizeof(binarySearchTreeOption));
+        }
+        //Insert into binary search tree
+        else if(actionName == binarySearchTreeOption[0]){
+            int no;
+            cout << "\n> How many nodes you wish to add to BST: ";
+            cin >> no;
+            
+            for(int i = 0; i < no; i++){
+                int value;
+                cout << "> " << (i+1) << ") Enter the value of new node: ";
+                cin >> value;
+                BST.insert(value);
+            }
+            
+            cout << "> Successfully Inserted. \n";
+            
+        }   
+        //Pre-order traversal
+        else if(actionName == binarySearchTreeOption[1]){
+            cout << "> Pre-order traversal: ";
+            BST.preOrderAccess(BST.getRoot());
+            cout << endl;
+        }
+        //In-order Traversal
+        else if(actionName == binarySearchTreeOption[2]){
+            cout << "> In-order traversal: ";
+            BST.inOrderAccess(BST.getRoot());
+            cout << endl;
+        }
+        //Post-order traversal
+        else if(actionName == binarySearchTreeOption[3]){
+            cout << "> Post-order traversal: ";
+            BST.postOrderAccess(BST.getRoot());
+            cout << endl;
+        }
+        //Find the smallest value
+        else if(actionName == binarySearchTreeOption[4]){
+            cout << "> Smallest value: ";
+            BST.smallestData();
+            cout << endl;
+        }
+        //Find the largest value
+        else if(actionName == binarySearchTreeOption[5]){
+            cout << "> Largest value: ";
+            BST.largestData();
+            cout << endl;
         }
         
+        //Unknown Operation
         else{
             printf("Error: Operation don't exist. \n");
         }
